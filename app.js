fa9c183182acba44dc132629496527f5ad8f3e2d
Pranjal Gupta <pgupta3_be19@thapar.edu>
2020-05-24 17:36:15 +0530

    updates in some rules

--- app.js
+++ app.js
@@ -12,38 +12,71 @@ var scores, roundScores, activePlayer, gamePlaying;
 
 init();
 
+var lastdice;
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
-    if (gamePlaying){var dice = Math.floor(Math.random()*6) + 1;
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice+'.png';
+    if (gamePlaying){
+        var dice1 = Math.floor(Math.random()*6) + 1;
+        var dice2 = Math.floor(Math.random()*6) + 1;
 
-    if (dice !== 1){
-        roundScores += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScores;
-    }else {
-        nextPlayer();
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1+'.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2+'.png';
 
-    }}
+        if (dice1 !== 1 && dice2 !== 1){
+                roundScores += dice1 + dice2;
+                document.querySelector('#current-' + activePlayer).textContent = roundScores;
+            }
+        else {
+                nextPlayer();
+    
+            }
+        
+        // if (dice === 6 && dice2 === 6){
+        //     scores[activePlayer] = 0;
+        //     document.querySelector('#score-' + activePlayer).textContent = '0';
+        //     nextPlayer();
+        // }
+        // else if (dice !== 1){
+        //     roundScores += dice;
+        //     document.querySelector('#current-' + activePlayer).textContent = roundScores;
+        // }
+        // else {
+        //     nextPlayer();
+
+        // }
+        // dice2 = dice;
+    }
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
     
-    if (gamePlaying){scores[activePlayer] += roundScores;
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    if (gamePlaying){
+        scores[activePlayer] += roundScores;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer]>=100){
-        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
-        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
-        gamePlaying = false;
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+        if (input){
+            winningScore = input;    
+        }else{
+            winningScore = 100;
+        }
+
+        if (scores[activePlayer]>=winningScore){
+            document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
+            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+            gamePlaying = false;
+        }
+        else{
+            nextPlayer();
+        }
     }
-    else{
-        nextPlayer();
-    }}
 
 });
 
@@ -59,7 +92,8 @@ function nextPlayer(){
         // document.querySelector('.player-0-panel').classList.remove('active');
         // document.querySelector('.player-1-panel').classList.add('active');
 
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -70,7 +104,8 @@ function init(){
     roundScores = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
